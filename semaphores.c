f03dd45aec4f892a2da61f3e8b8ed6c7d8313377
Kacper Klassa <kacperk143@gmail.com>
2021-12-13 20:17:12 +0100

    add buffer state printing

--- semaphores.c
+++ semaphores.c
@@ -4,7 +4,6 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define BUFFERS 3
 #define BUFFER_SIZE 2
 #define DELAY   1
 
@@ -14,16 +13,6 @@ int buffer_2[BUFFER_SIZE];
 int count_2 = 0;
 int buffer_3[BUFFER_SIZE];
 int count_3 = 0;
-
-int buffers[BUFFERS][BUFFER_SIZE];
-int counts[BUFFERS];
-
-void initBuffers() {
-    int i;
-    for (i = 0; i< BUFFERS;i++) {
-        counts[i] = 0;
-    }
-}
 
 void insertIntoBuffer(int x, int* queue, int* count) {
     queue[*count] = x;
@@ -52,11 +41,6 @@ void printBuffer(int* queue, int* count) {
 sem_t sem_1_rw, sem_1_empty, sem_1_full;
 sem_t sem_2_rw, sem_2_empty, sem_2_full;
 sem_t sem_3_rw, sem_3_empty, sem_3_full;
-
-sem_t sems_readwrite[BUFFERS];
-sem_t sems_empty[BUFFERS];
-sem_t sems_full[BUFFERS];
-
 
 void* producer_1() {
     for(;;) {
@@ -133,7 +117,6 @@ void* producer_2() {
 }
 
 void* consumer_1() {
-    int i;
     for(;;) {
         sem_wait(&sem_1_full);
         sem_wait(&sem_1_rw);
@@ -149,7 +132,6 @@ void* consumer_1() {
 }
 
 void* consumer_2() {
-    int i;
     for(;;) {
         sem_wait(&sem_2_full);
         sem_wait(&sem_2_rw);
@@ -165,7 +147,6 @@ void* consumer_2() {
 }
 
 void* consumer_3() {
-    int i;
     for(;;) {
         sem_wait(&sem_3_full);
         sem_wait(&sem_3_rw);
@@ -182,29 +163,6 @@ void* consumer_3() {
 
 int main()
 {
-    /* initBuffers();
-
-    int i;
-    for (i = 0;i < BUFFERS; i++) {
-        sem_init(&sems_readwrite[i], 0, 1);
-        sem_init(&sems_empty[i], 0, 1);
-        sem_init(&sems_full[i], 0, 0);
-    }
-
-    pthread_t threads[2*BUFFERS-1]; // BUFFERS - 1 producers, BUFFERS consumers
-
-
-    int j;
-    for (j = 0; j < 2*BUFFERS-1; j++) {
-        pthread_join(threads[i], NULL);
-    }
-
-    int k;
-    for (k = 0; k < BUFFERS; k++) {
-        sem_destroy(&sems_readwrite[i]);
-        sem_destroy(&sems_empty[i]);
-        sem_destroy(&sems_full[i]);
-    }*/
 
     sem_init(&sem_1_rw, 0, BUFFER_SIZE);
     sem_init(&sem_1_empty, 0, BUFFER_SIZE);
